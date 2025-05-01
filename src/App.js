2cb8205325782b25c75c7026e392a6d90293e092
luna1125 <35061134+rita1125@users.noreply.github.com>
2025-05-01 23:39:14 +0800

    Feat/add words (#2)
    * init main
    
    * 增加文字

---
+++ src/App.js
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px' }}>
+      <h1>Hello from React on Cloud Run! Love you ~</h1>
+    </div>
+  );
+}
+export default App;
