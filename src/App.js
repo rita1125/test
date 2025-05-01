ce97b1a969405b1dc86f25ede21484e24afd5892
rita1125 <chenyenzu1125@gmail.com>
2025-05-01 22:17:34 +0800

    feat分支加文字

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '40px' }}>
-      <h1>Hello from React on Cloud Run!</h1>
+      <h1>Hello from React on Cloud Run! Love you ~ 因為你休息過後總是會再前進，就算是只有走一步也很棒哦!</h1>
     </div>
   );
 }
